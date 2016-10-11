b6d659e571309469ec881dbc91f316462200e84f
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-11 11:29:28 +0200

    One-to-Many: Models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010093620) do
+ActiveRecord::Schema.define(version: 20161011090954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "home_type"
+    t.string   "room_type"
+    t.integer  "accommodate"
+    t.integer  "bedroom_count"
+    t.integer  "bathroom_count"
+    t.string   "listing_name"
+    t.text     "description"
+    t.string   "address"
+    t.boolean  "has_tv"
+    t.boolean  "has_kitchen"
+    t.boolean  "has_airco"
+    t.boolean  "has_heating"
+    t.boolean  "has_internet"
+    t.decimal  "price"
+    t.boolean  "active"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +54,5 @@ ActiveRecord::Schema.define(version: 20161010093620) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "rooms", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+User.create(email: "miriam@codaisseurbnb.com", password: "secret")
+User.create(email: "ben@codaisseurbnb.com", password: "secret")
