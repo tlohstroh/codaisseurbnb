22228c46eec0c8a3648b8981eef29cae5fb3c4cd
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-11 16:15:01 +0200

    Add many-to-many association between room and theme

---
+++ app/models/theme.rb
@@ -0,0 +1,4 @@
+class Theme < ApplicationRecord
+  has_and_belongs_to_many :rooms
+  validates :name, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011090954) do
+ActiveRecord::Schema.define(version: 20161011131518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,19 @@ ActiveRecord::Schema.define(version: 20161011090954) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
+  end
+
+  create_table "rooms_themes", id: false, force: :cascade do |t|
+    t.integer "room_id"
+    t.integer "theme_id"
+    t.index ["room_id"], name: "index_rooms_themes_on_room_id", using: :btree
+    t.index ["theme_id"], name: "index_rooms_themes_on_theme_id", using: :btree
+  end
+
+  create_table "themes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
