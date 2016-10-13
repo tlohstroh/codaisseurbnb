9dd2ae15cca0ff2515b7d227ed992497e98254b8
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-13 14:11:40 +0200

    Create Photo: Model

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :rooms
   resources :profiles, only: [:new, :edit, :create, :update]
+  resources :photos
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,10 @@
 # User.create(email: "miriam@codaisseurbnb.com", password: "secret")
 # User.create(email: "ben@codaisseurbnb.com", password: "secret")
 
-User.delete_all
-Room.delete_all
+Photo.delete_all
 Theme.delete_all
+Room.delete_all
+User.delete_all
 
 modern_and_minimal = Theme.create(name: "Modern and Minimal")
 sleek_and_sophisticated = Theme.create(name: "Sleek and Sophisticated")
@@ -28,8 +29,23 @@ miriam = User.create(email: "miriam@codaisseurbnb.com", password: "abcd1234")
 wouter = User.create(email: "wouter@codaisseurbnb.com", password: "abcd1234")
 matt = User.create(email: "matt@codaisseurbnb.com", password: "abcd1234")
 
-Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "You will never forget your stay!", description: "Normcore meh put a bird on it man braid mumblecore, locavore synth swag kogi helvetica ugh health goth twee raw denim readymade.", address: "Colon 58, 1B, 11100 Cadiz (Spain)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: miriam, themes: [modern_and_minimal, sleek_and_sophisticated])
+# Photos
+photo1 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359497/gevel_hondop.jpg")
+photo2 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359496/een_a7v6yf.jpg")
+photo3 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359498/woonkamer_mlylmt.jpg")
 
-Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "A place in the middle of Holland", description: "Freegan yuccie everyday carry tote bag you probably haven't heard of them godard.", address: "Daguerrestraat 23, 1054TG Utrecht (Netherlands)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: wouter, themes: [warm_and_cozy, a_mans_touch])
+photo4 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359498/woon-en-eetkamer_aw16ii.jpg")
+photo5 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359497/keuken_n38thf.jpg")
+photo6 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359497/terras_vrgviu.jpg")
 
-Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "Great for a Weekend", description: "Meditation tattooed lumbersexual street art truffaut pour-over, kale chips pitchfork. Readymade snackwave man bun, photo booth tilde wolf occupy fashion axe VHS authentic PBR&B typewriter. Vape everyday carry meditation try-hard hammock unicorn.", address: "Tottenham Court Road 14, W1T 1JY London (UK)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: matt, themes: [elegant_and_ornate, white_washed])
+photo7 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359497/eetplaats_wo4wam.jpg")
+photo8 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476359966/slaapkamer-1_jhvitg.jpg")
+photo9 = Photo.create(remote_image_url: "http://res.cloudinary.com/ddpouudhk/image/upload/v1476360023/badkamer_vxadky.jpg")
+
+
+
+Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "You will never forget your stay!", description: "Normcore meh put a bird on it man braid mumblecore, locavore synth swag kogi helvetica ugh health goth twee raw denim readymade.", address: "Colon 58, 1B, 11100 Cadiz (Spain)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: miriam, themes: [modern_and_minimal, sleek_and_sophisticated], photos: [photo1, photo2, photo3])
+
+Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "A place in the middle of Holland", description: "Freegan yuccie everyday carry tote bag you probably haven't heard of them godard.", address: "Daguerrestraat 23, 1054TG Utrecht (Netherlands)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: wouter, themes: [warm_and_cozy, a_mans_touch], photos: [photo4, photo5, photo6])
+
+Room.create(home_type: "House", room_type: "Shared", accommodate: 2, bedroom_count: 2, bathroom_count: 3, listing_name: "Great for a Weekend", description: "Meditation tattooed lumbersexual street art truffaut pour-over, kale chips pitchfork. Readymade snackwave man bun, photo booth tilde wolf occupy fashion axe VHS authentic PBR&B typewriter. Vape everyday carry meditation try-hard hammock unicorn.", address: "Tottenham Court Road 14, W1T 1JY London (UK)", has_tv: false, has_kitchen: true, has_airco: false, has_heating: false, has_internet: true, price: 100, active: true, user: matt, themes: [elegant_and_ornate, white_washed], photos: [photo7, photo8, photo9])
