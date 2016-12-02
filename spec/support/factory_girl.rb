d075f9f48e1746cd42c4d2a1c0b194b60cecf713
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-12-02 16:26:43 +0100

    some comments on Rspec

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,3 +1,8 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
+
+# https://read.codaisseur.com/topics/day-10-rails-testing/articles/02-fixtures-and-factories
+
+# Enable the autoloading of the support directory by uncommenting the following line in your spec/rails_helper.rb:
+# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
